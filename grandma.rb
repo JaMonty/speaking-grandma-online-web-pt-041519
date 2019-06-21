6d86578e508587a036717bdc60bca169eebf9814
Jason  Montes <jasone.montes@gmail.com>
2019-06-21 15:49:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
+elsif phrase != phrase.upcase
+  
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
